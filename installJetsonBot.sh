7e61a928d31a36de77d0402cd720263c56deb6bc
Robert Hamilton <rghamilton3@gmail.com>
2017-05-19 23:40:22 -0500

    Remove out of date packages

--- installJetsonBot.sh
+++ installJetsonBot.sh
@@ -5,9 +5,8 @@ _dir="jetsonbot/src"
 # Check to make sure that the JetsonBot catkin directory exists
 [ ! -d "$_dir" ] && { echo "Error: Directory $_dir not found.\nPlease run createJetsonBotWS.sh to create the JetsonBot catkin workspace."; exit 2; }
 # Install TurtleBot from repository
-sudo apt-get install ros-indigo-turtlebot ros-indigo-turtlebot-apps \
-ros-indigo-turtlebot-interactions ros-indigo-rocon-remocon \
-ros-indigo-rocon-qt-library ros-indigo-ar-track-alvar-msgs \
+sudo apt-get install ros-kinetic-turtlebot ros-kinetic-turtlebot-apps \
+ros-kinetic-turtlebot-interactions ros-indigo-ar-track-alvar-msgs \
 ros-indigo-collada-urdf -y
 
 cd ~/jetsonbot/src
